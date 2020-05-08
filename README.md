759ec43f405696710e352a9cf671a66122167d81
github username <kateapault@gmail.com>
2020-05-08 16:42:34 -0400

    project euler problem 6

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ There's one file per problem, and each file is named in the following format:
 [problem number]-problem-name
 
 Leetcode problems are mainly in Ruby and Project Euler problems are in Python.
+
+Leetcode problems completed (in here): 2
+Project Euler problems completed (in here): 1
--- project-euler/6-sum-square-difference.py
+++ project-euler/6-sum-square-difference.py
@@ -8,7 +8,15 @@
 # Find the difference between the sum of the squares of the first one 
 # hundred natural numbers and the square of the sum.
 
-sum = 10 * (1 + 10) / 2 = 55
+def sum_of_squares_to_n(n):
+    return(n*(n+1)*(2*n+1) / 6)
 
-# 1^2 + 2^2 + ... + 15^2 = 1240
-# sum = 15 * (1 + 15) / 2 = 120
+def sum_to_n(n):
+    return(n*(n+1)/2)
+
+def sum_square_difference(n):
+    sum_squares = sum_of_squares_to_n(n)
+    square_sum = sum_to_n(n)**2
+    return(square_sum - sum_squares)
+
+print(sum_square_difference(100))
